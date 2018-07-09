ce5b2546502cd5d4d3f41f296cf7d83b53544005
yousuf zhang <zhangshuai7969@163.com>
2018-07-10 07:47:39 +0800

    购物车

---
+++ src/app/model/cart-item.ts
@@ -0,0 +1,9 @@
+export class CartItem {
+  /**产品链接*/
+  href: string;
+  productNo: string;
+  productName: string;
+  count: number;
+  amount: number;
+  cover: string;
+}
--- src/app/routes/wechat/components/shopping-cart/shopping-cart.component.ts
+++ src/app/routes/wechat/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { yuan } from '@delon/util';
+import { CartItem } from '../../../../model/cart-item';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ShoppingCartComponent implements OnInit {
-
+  data: CartItem[];
+  yuan = yuan;
   constructor() { }
 
   ngOnInit() {
+    this.getData();
+  }
+
+  getData(): void {
+    this.data = new Array(5).fill({}).map((i, index) => {
+      return {
+        href: 'http://ant.design',
+        productNo: `rxr-10-100 ${index}`,
+        productName: 'product',
+        count: index,
+        amount: 12.24,
+        cover: 'https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'
+      };
+    });
   }
 
 }
